5c7f4512689f32befe97ad6325ba77bb36311877
2je0 <82891332+2je0@users.noreply.github.com>
2022-06-12 20:43:02 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,38 +1,22 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 int main()
 {
     // freopen("input.txt", "r", stdin);
     int arr[9];
-    int sum = 0;
     for (int i = 0; i < 9; i++)
     {
         scanf("%d", &arr[i]);
-        sum += arr[i];
     }
     sort(arr, arr + 9);
-    int target = sum - 100;
-    pair<int, int> deleteIdx;
-    for (int i = 0; i < 8; i++)
+    do
     {
-        for (int j = i + 1; j < 9; j++)
-        {
-            if (arr[i] + arr[j] == target)
-            {
-                deleteIdx = {i, j};
-                break;
-            }
-        }
-    }
-    for (int i = 0; i < 9; i++)
-    {
-        if (i == deleteIdx.first || i == deleteIdx.second)
-            continue;
-
+        int sum = accumulate(arr, arr + 7, 0);
+        if (sum == 100)
+            break;
+    } while (next_permutation(arr, arr + 9));
+    for (int i = 0; i < 7; i++)
         printf("%d\n", arr[i]);
-    }
 
     return 0;
 }
