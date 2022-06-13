8b8ac780914ce522a4937c8e74c467f67c84583f
2je0 <82891332+2je0@users.noreply.github.com>
2022-06-13 22:13:23 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 208 ms, Memory: 28652 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define MAX 100004
+using namespace std;
+string arr[MAX];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    map<string, int> strkey;
+    map<int, string> intkey;
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        string input;
+        cin >> input;
+        strkey[input] = i;
+        intkey[i] = input;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        string input;
+        cin >> input;
+        int isdigit = atoi(input.c_str());
+        if (isdigit)
+            cout << intkey[isdigit] << '\n';
+        else
+            cout << strkey[input] << '\n';
+    }
+    return 0;
+}
