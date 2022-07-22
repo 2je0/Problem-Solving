54c9f0705db0624a89d76e0b2b2617211576eb22
Lee Jeyoung <82891332+2je0@users.noreply.github.com>
2022-07-22 23:50:23 +0900

    [Silver II] Title: 꽃길, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.cc
+++ 백준/Silver/14620. 꽃길/꽃길.cc
@@ -18,17 +18,6 @@ void calcPrice(int x, int y) {
     price[x][y] = res;
 }
 
-void dfs(int x, int y, int level, int sum) {
-    if (level == 3) {
-        minn = min(minn, sum);
-    }
-    for (int i = x; i < n - 1; i++) {
-        for (int j = y; j < n - 1; j++) {
-            if (i - x + j - y < 3) continue;
-            dfs(i, j, level + 1, sum + price[i][j]);
-        }
-    }
-}
 void dfss(vector<pair<int, int>> v, int sum) {
     if (v.size() == 3) {
         minn = min(minn, sum);
@@ -47,10 +36,6 @@ void dfss(vector<pair<int, int>> v, int sum) {
             }
             if (skip) continue;
             v.push_back({i, j});
-            // for (auto& ele : v) {
-            //     cout << ele.first << ',' << ele.second << endl;
-            // }
-            // cout << endl;
             dfss(v, sum + price[i][j]);
             v.pop_back();
         }
@@ -76,12 +61,6 @@ int main() {
             v.pop_back();
         }
     }
-    // for (int i = 1; i < n - 1; i++) {
-    //     for (int j = 1; j < n - 1; j++) {
-    //         printf("%4d", price[i][j]);
-    //     }
-    //     cout << endl;
-    // }
     cout << minn;
 
     return 0;
