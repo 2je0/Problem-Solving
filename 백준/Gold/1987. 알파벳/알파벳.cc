b5e760bc64ccf29168b5dba1d13c031e23a66647
Lee Jeyoung <82891332+2je0@users.noreply.github.com>
2022-07-27 01:49:41 +0900

    [Gold IV] Title: 알파벳, Time: 484 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,52 +1,39 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-int check[26];
-vector<string> arr;
-int r, c, res=0;
-int d_x[4] = { 1,-1,0,0 };
-int d_y[4] = { 0,0,1,-1 };
-void DFS(int i, int j, int cnt) {
-	//cout << i << " " << j << " " << cnt << endl;
-	int idx = arr[i][j] - 'A'; // arr[0] -> A: 0이 idx
+char arr[20][20];
+int alpha[26];
+int visited[20][20];
 
-	if (check[idx] == 1) { //한번 방문한 적이 있던 곳이라면
-		res = max(cnt, res);
-		//cout << "return" << endl;
-		return;
-	}
-	else {
-		//cout << arr[i][j] << " ";
-		//cout << i << " " << j << endl;
-		for (int k = 0; k < 4; k++) {
-			int new_i = i + d_x[k];
-			int new_j = j + d_y[k];
-			if (new_i >= 0 && new_i < r && new_j >= 0 && new_j < c) {
-				check[idx] = 1;
-				DFS(new_i, new_j, cnt + 1);
-				check[idx] = 0;
-			}
+int r, c;
+int maxx = -1;
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
 
-		}
-	}
+void dfs(int x, int y, int depth) {
+    maxx = max(maxx, depth);
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if (nx < 0 || nx >= r || ny < 0 || ny >= c || visited[nx][ny]) continue;
+        int alphaIdx = arr[nx][ny] - 'A';
+        if (alpha[alphaIdx]) continue;
+        visited[nx][ny] = 1;
+        alpha[alphaIdx] = 1;
+        dfs(nx, ny, depth + 1);
+        visited[nx][ny] = 0;
+        alpha[alphaIdx] = 0;
+    }
 }
-
 int main() {
-	//freopen("input.txt", "rt", stdin);
+    cin >> r >> c;
+    for (int i = 0; i < r; i++) {
+        scanf("%s", arr[i]);
+    }
+    visited[0][0] = 1;
+    int alphaIdx = arr[0][0] - 'A';
+    alpha[alphaIdx] = 1;
+    dfs(0, 0, 1);
+    printf("%d", maxx);
 
-	cin >> r >> c;
-	cin.ignore();
-
-	arr.resize(r);
-	for (int i = 0; i < r; i++) {
-		getline(cin, arr[i]);
-	}
-	DFS(0, 0, 0);
-	cout << res << endl;
-
-
-	return 0;
+    return 0;
 }
