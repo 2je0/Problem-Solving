1587b82cf3c652d37d0da2c2192b5ddd1a86045d
Lee Jeyoung <82891332+2je0@users.noreply.github.com>
2022-08-17 01:33:45 +0900

    [Gold III] Title: 소수의 연속합, Time: 40 ms, Memory: 9128 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 #define FIO ios ::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MAX 4000001
 using namespace std;
 int n, cnt = 0, sum = 0;
-deque<int> dq;
-bool isPrime(int n) {
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) return false;
-    }
-    return true;
-}
+vector<int> v;
+bool arr[MAX];
 
 int main() {
     FIO;
     cin >> n;
-
+    for (int i = 2; i < MAX; i++) {
+        if (arr[i]) continue;
+        for (int j = i * 2; j < MAX; j += i) arr[j] = 1;
+    }
+    for (int i = 2; i < MAX; i++) {
+        if (!arr[i]) v.push_back(i);
+    }
     int left = 0;
-    for (int i = 2; i <= n; i++) {
-        if (!isPrime(i)) continue;
-        dq.push_back(i);
-        sum += i;
-        while (dq.size() && sum > n) {
-            sum -= dq.front();
-            dq.pop_front();
+    for (int i = 0; i < v.size(); i++) {
+        sum += v[i];
+        while (left <= i && sum > n) {
+            sum -= v[left++];
         }
         if (sum == n) cnt++;
     }
