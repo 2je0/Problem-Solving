860b6a604ddf9a49699f9a4f6c14e6d53b026b74
Lee Jeyoung <82891332+2je0@users.noreply.github.com>
2022-08-17 23:59:24 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 12 ms, Memory: 2508 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
+#define FIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 int n, lft = 0, right = 0;
 long long sum = 0;
 int arr[100001];
 bool check[100001];
 int main() {
+    FIO;
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
