aae8be5f0a21f1fa32de0048b20fe67d8e3f5f72
Lee Jeyoung <82891332+2je0@users.noreply.github.com>
2022-08-24 18:49:58 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+char a;
+void IO() {
+    FIO;
+    cin >> a;
+}
+
+int main() {
+    IO();
+    printf("%d", a);
+    return 0;
+}
