5bf62991a8483c68ce2c07c01ec719fd03eb9c80
Lee Jeyoung <82891332+2je0@users.noreply.github.com>
2022-09-06 22:56:08 +0900

    [Silver I] Title: 기타 레슨, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.cc
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -20,6 +20,9 @@ bool check(int len) {
         return true;
 }
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
