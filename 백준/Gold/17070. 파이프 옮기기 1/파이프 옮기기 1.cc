42d7f286d97efb17278b20bb42ba414f8943ecfe
Lee Jeyoung <82891332+2je0@users.noreply.github.com>
2022-09-20 22:11:09 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAXN 17
+int n, wall[MAXN][MAXN];
+long long cnt[MAXN][MAXN][3];
+int main() {
+    ios_base::sync_with_stdio(0), cout.tie(0), cin.tie(0);
+    cin >> n;
+    cnt[1][2][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> wall[i][j];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (wall[i][j]) {
+                // cout << cnt[i][j][0] + cnt[i][j][1] + cnt[i][j][2] << ' ';
+                continue;
+            }
+            if (j > 1) {
+                cnt[i][j][0] += cnt[i][j - 1][0];
+                cnt[i][j][0] += cnt[i][j - 1][2];
+            }
+            if (i > 1) {
+                cnt[i][j][1] += cnt[i - 1][j][1];
+                cnt[i][j][1] += cnt[i - 1][j][2];
+            }
+            if (i > 1 && j > 1 && wall[i - 1][j] == 0 && wall[i][j - 1] == 0) {
+                cnt[i][j][2] += cnt[i - 1][j - 1][0];
+                cnt[i][j][2] += cnt[i - 1][j - 1][1];
+                cnt[i][j][2] += cnt[i - 1][j - 1][2];
+            }
+            // cout << cnt[i][j][0] + cnt[i][j][1] + cnt[i][j][2] << ' ';
+        }
+        // cout << '\n';
+    }
+    cout << cnt[n][n][0] + cnt[n][n][1] + cnt[n][n][2];
+    return 0;
+}
