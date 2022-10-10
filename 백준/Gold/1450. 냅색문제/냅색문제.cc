1002da4e62c3af0ed1c55ea50c7ca8c3d77144af
Lee Jeyoung <82891332+2je0@users.noreply.github.com>
2022-10-11 01:29:45 +0900

    [Gold I] Title: 냅색문제, Time: 4 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.cc
+++ 백준/Gold/1450. 냅색문제/냅색문제.cc
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long n, c, cnt;
-vector<long long> v1, v2;
+int n, c, cnt;
+vector<int> v1, v2;
 int arr[31];
-void rec(vector<long long>& v, int level, int end, long long sum) {
+void rec(vector<int>& v, int level, int end, int sum) {
     if (level == end) {
         v.push_back(sum);
         return;
@@ -12,6 +12,23 @@ void rec(vector<long long>& v, int level, int end, long long sum) {
     if (sum + arr[level] <= c) {
         rec(v, level + 1, end, sum + arr[level]);
     }
+}
+void test() {
+    cout << "arr : ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+    cout << "v1 : ";
+    for (auto& ele : v1) {
+        cout << ele << ' ';
+    }
+    cout << '\n';
+    cout << "v2 : ";
+    for (auto& ele : v2) {
+        cout << ele << ' ';
+    }
+    cout << "\ncnt : ";
 }
 int main() {
     cin >> n >> c;
@@ -22,10 +39,11 @@ int main() {
     rec(v2, (n / 2), n, 0);
     sort(v2.begin(), v2.end());
     for (auto& ele : v1) {
-        long long b = c - ele;
-        long long k = upper_bound(v2.begin(), v2.end(), b) - v2.begin();
+        int b = c - ele;
+        int k = upper_bound(v2.begin(), v2.end(), b) - v2.begin();
         cnt += k;
     }
+    // test();
     cout << cnt;
 
     return 0;
